5643835888e77f4c3af6f8d4f9281df914c1672c
lakshmanan <laksys@gmail.com>
2026-04-26 13:33:49 +0530

    Initial commit

---
+++ sb-discovery/src/main/java/in/laksys/discovery/App.java
@@ -0,0 +1,16 @@
+package in.laksys.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(App.class, args);
+	}
+
+}
